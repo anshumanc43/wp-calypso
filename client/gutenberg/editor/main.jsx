82e65dff217c792d6a05e50c7f559568f2f04662
mmtr <miguel.torres@automattic.com>
2019-01-10 17:24:15 +0100

    Replace the source of the Core video blocks with the VideoPress URL

--- client/gutenberg/editor/main.jsx
+++ client/gutenberg/editor/main.jsx
@@ -25,7 +25,6 @@ import {
 } from 'state/data-getters';
 import { getHttpData } from 'state/data-layer/http-data';
 import { translate } from 'i18n-calypso';
-import './hooks'; // Needed for integrating Calypso's media library (and other hooks)
 import isRtlSelector from 'state/selectors/is-rtl';
 import refreshRegistrations from '../extensions/presets/jetpack/utils/refresh-registrations';
 import { getSiteOption, getSiteSlug } from 'state/sites/selectors';
